4b830a126019954949f3320e7886f9f6f9c52b9d
August Wang <august.wang.tpe@gmail.com>
2024-11-07 21:30:58 +0800

    基礎公式區塊完成

--- pdckd_v2/src/App.vue
+++ pdckd_v2/src/App.vue
@@ -53,6 +53,12 @@ onMounted(() => {
     initTabs();
     initTooltips();
 })
+
+function calculateRisk() {
+    const risk = 0.45 + 2.23 * Sex.value - 0.06 * PDage.value - 0.09 * BW.value - 0.77 * peritonitits.value - 2.64 * Exit.value - 0.19 * PDuration.value + 0.05 * aWCCR.value + 2.11 * bKTV.value + 2.76 * underly5.value - 3.53 * periRa2.value - 3.38 * ENDnew2.value + 20.31 * ENDnew3.value;
+    return Math.round(risk * 100) / 100;
+}
+
 </script>
 
 <template>
@@ -60,7 +66,14 @@ onMounted(() => {
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
               <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">兒童腹膜透析預測模型</h1>
-              <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+              <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                我們結合專利申請之 3D 科技列印與智慧數位雲端智能，以螺旋式層次多
+                維設計，打造兒童腹膜透析沉浸式智慧互動學習系統。混成式實境腹膜透
+                析學習系統使用實體 3D 模型進行擬真實境操作、數位 3D 模組進行即時雲
+                端教學，雲端情境模擬測驗檢視操作觀念及能力，並結合擴增 3D 擬境達
+                成個人化的沉浸式智能互動學習，以五大特色建構創新之精準兒童腹膜透
+                析智能教育系統。
+              </p>
               <a href="#section01" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                   開始測驗 
                   <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -79,7 +92,14 @@ onMounted(() => {
               <section class="bg-white dark:bg-gray-900">
                 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                    
-                    <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                    <p class="mb-8 lg:mb-16 font-light text-left text-gray-500 dark:text-gray-400 sm:text-xl">
+                        以 3D 列印實體視覺化腹膜透析操作過程中，治療空間的重點物件，
+                        包含病人模型、磅秤、微波爐、支撐桿、透析液雙聯袋、冷氣機、洗手台、
+                        桌子、椅子、床、電風扇、牆壁(含門)、牆壁(含窗)、牆壁、燈、綠色圖示、
+                        音源(可為喇叭、手機、音響)。物件可彼此結合組裝成一連續空間，其中，
+                        實體視覺化的重要特點有我們團隊申請發明專利的關鍵組裝技術，以及可
+                        以進行衛教測試的視覺化開關設計。
+                    </p>
                     <form action="#">
                       <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-6">
                         <div>
@@ -108,18 +128,36 @@ onMounted(() => {
               </section>
           </div>
           <div class="grid grid-cols-2 gap-4 mt-8">
-              <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
-              <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+                <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <img src="https://vueproject.s3.us-west-2.amazonaws.com/test.jpg" alt="mockup">
+                </div>
           </div>
+          
       </div>
   </section>
   <section class="bg-white dark:bg-gray-900">
       <div class="gap-16 items-start py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400 ">
-              <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">公式計算</h2>
-              <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-              <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
-          </div>
+          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">公式計算</h2>
+            <p class="mb-4">
+                <strong class="font-extrabold text-lg">PD-PET Trends Group Models 和 Canonical Analysis 是在資料分析中常用的技術，用於從多變數資料中提取關鍵趨勢，並分析不同變數之間的關聯性。這些方法特別適合用於處理大量且複雜的資料，並將資料中潛在的模式視覺化，幫助識別關鍵的趨勢和異常。</strong>
+            </p>
+            <div class="pt-1"></div>
+            <p>
+                <strong>基本原理</strong> <br>
+                <strong>PD-PET Trends Group Models：</strong>這個方法聚焦於趨勢的識別與分組，通過將資料分類成不同的群組來找出相似的趨勢或特徵模式。例如，將多個變數依相似趨勢分組，以更容易觀察不同群組之間的相對變化。
+                <br>
+                <strong>Canonical Analysis（典型相關分析）：</strong>這是一種多變數統計分析技術，目的是找出兩組變數之間的最大相關性。透過將兩組變數轉換為線性組合，該方法能夠找出最能表達兩組資料間關係的配對，並提供不同變數之間的關聯強度。
+            </p>
+            <p>
+                <strong>好處</strong>
+            </p>
+            <ul>
+                <li>降低資料維度：這些方法能夠將高維度的複雜資料簡化成少數幾個重要的趨勢和關聯，提高解讀效率。</li>
+                <li>增強資料解釋力：PD-PET 和 Canonical Analysis 有助於識別不同變數之間的相互關聯，幫助分析師深入了解資料背後的關聯性結構。</li>
+                <li>精確趨勢預測：這些技術提供了更好的趨勢預測能力，有助於組織在未來決策時更準確地預測變數之間的互動影響。</li>
+            </ul>
+            </div>
           <form action="#">
                 <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-6">
                     <div>
@@ -187,10 +225,12 @@ onMounted(() => {
   <section class="bg-white dark:bg-gray-900">
       <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           
-          <div class="grid grid-cols-2 gap-4 mt-8">
-              <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
-              <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+        <div class="grid grid-cols-2 gap-4 mt-8">
+                <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <img src="https://vueproject.s3.us-west-2.amazonaws.com/sangharsh-lohakare-Iy7QyzOs1bo-unsplash.jpg" alt="mockup">
+                </div>
           </div>
+            
           <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
               <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">精準基因分析</h2>
               <form action="#">
@@ -257,12 +297,22 @@ onMounted(() => {
             </div>
             <!-- Modal body -->
             <div class="p-4 md:p-5 space-y-4">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                <!-- 這邊想要放一個計算風險的結果利用calculateRisk()這個function -->
+                <p :class="{'text-red-500': calculateRisk() > 0.5, 'text-green-500': calculateRisk() <= 0.5}" class="text-base leading-relaxed dark:text-gray-400">
+                    計算風險的結果是：{{ calculateRisk() }}
                 </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                <p v-if="calculateRisk() > 0.5" class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    高風險的人群應該注意保持健康生活方式，定期檢查身體狀況，並遵循醫生的建議進行治療。
                 </p>
+                <p v-else class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    低風險的人群應該保持健康生活方式，定期檢查身體狀況，並遵循醫生的建議進行治療。
+                </p>
+
+
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    <strong>風險預測基於以下公式計算：</strong>
+                </p>
+                <img src="https://vueproject.s3.us-west-2.amazonaws.com/cal.png" alt="Risk Calculation Formula" class="mx-auto">
             </div>
             <!-- Modal footer -->
             <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
